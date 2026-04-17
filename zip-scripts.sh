7e418379e2407abea871a347df5eb5f510265375
root <root@ip-172-31-47-140.eu-north-1.compute.internal>
2026-04-17 09:48:23 +0000

    update file

--- zip-scripts.sh
+++ zip-scripts.sh
@@ -8,3 +8,9 @@ echo "Hello from Semaphore UI" > $OUTPUT_DIR/sample.txt
 zip $OUTPUT_DIR/sample.zip $OUTPUT_DIR/sample.txt
 
 echo "Zip stored at $OUTPUT_DIR/sample.zip"
+
+echo "This is the vm"
+whoami
+pwd
+ls -a
+
